d5c9a7cd73bf75e813f49f933f12ef65cd8841cd
Celybot <youancece@gmail.com>
2023-01-20 08:02:30 -0500

    solved word problems and test

--- exercises.js
+++ exercises.js
@@ -1,47 +1,114 @@
 // Question 1
-const isRealPalindrome = () => {
-
+const isRealPalindrome = (str) => {
+let newArr = []
+str = str.replace(/[^\w]/g,"")
+console.log(str)
+let lowerCasedStr = str.toLowerCase().split('')
+  for(let i = lowerCasedStr.length-1; i >= 0; i--){
+    // console.log(lowerCasedStr[i])
+    newArr.push(lowerCasedStr[i])
+  }
+  if(newArr.join('') === lowerCasedStr.join('')){
+    return true
+  }else{
+    return false
+  }
 };
+console.log(isRealPalindrome("Madam, I'm Adam"))
 
 // Question 2
-const runningTotal = () => {
+const runningTotal = (arr) => {
+  let newArr = []
+  let num = 0;
 
+  for(let i =0; i<arr.length; i++){
+    console.log(num += arr[i])
+    newArr.push(num)
+  }
+  return newArr
 };
+
+//console.log(runningTotal([2, 5, 13]));             // [2, 7, 20]
+//console.log(runningTotal([14, 11, 7, 15, 20]));    // [14, 25, 32, 47, 67]
 
 // Question 3
-const swap = () => {
-
+const swap = (str) => {
+ 
+  
 };
+ console.log(swap('Four score and seven.'))
 
 // Question 4
-const wordSizes = () => {
-
+const wordSizes = (str) => {
+  let obj = {}
+  str = str.split(' ')
+  for(let i = 0; i < str.length; i++){
+    console.log(str[i])
+    if(obj[str[i].length]){
+      obj[str[i].length]++
+    }else{
+      obj[str[i].length] = 1
+    }
+  }
+  console.log(obj)
 };
+
+// console.log(wordSizes('Four score and seven.'))                      // { "3": 1, "4": 1, "5": 1, "6": 1 }
+// console.log(wordSizes('Hey diddle diddle the cat and the fiddle!'));         // { "3": 5, "6": 1, "7": 2 }
+// console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
 
 // Question 5
-const union = () => {
+const union = (arr1,arr2) => {
+  let concatedArr = arr1.concat(arr2)
+  let results = [...new Set(concatedArr)]
 
+  console.log(results)
 };
+console.log(union([1, 3, 5], [3, 6, 9]));     // [1, 3, 5, 6, 9]
+console.log(union([2, 2, 2, 2], [10, 5, 2])); // [2, 10, 5]
 
 // Question 6
-const firstRecurring = () => {
-
+const firstRecurring = (str) => {
+    for (let i = 0; i < string.length; i++) {
+      for (let j = i + 1; j < string.length; j += 1) {
+        if (string[i] === string[j]) {
+          return string[i]
+        }
+      }
+    }return ""
 };
+console.log(firstRecurring('reuben'));           // "e"
+console.log(firstRecurring('anne'));             // "n"
+console.log(firstRecurring('restaurant'));       // "r"
 
 // Question 7
-const showMultiplicativeAverage = () => {
-
+const showMultiplicativeAverage = (arr) => {
+let newStr = ""
+let intialValue = 1
+ let multipliedValue = arr.reduce((accumulator, currentValue) => accumulator * currentValue, intialValue /arr.length)
+ return newStr += Math.round(multipliedValue * 1000)/1000
 };
+console.log(showMultiplicativeAverage([3, 5]));                   // "7.500"
+console.log(showMultiplicativeAverage([2, 5, 7, 11, 13, 17]));    // "28361.667"
 
 // Quetsion 8
-const multiplyList = () => {
+const multiplyList = (arr1, arr2) => {
+  return arr1.map((element,i) => element *= arr2[i]);
 };
+
+//console.log(multiplyList([3, 5, 7], [9, 10, 11]))
 
 // Question 9
-const sequence = () => {
-
+const sequence = (num) => {
+  let newArr = []
+  for(let i = 1; i<=num; i++){
+    newArr.push(i)
+  }
+  return newArr
 };
-
+//console.log(sequence(5));    // [1, 2, 3, 4, 5]
+//console.log(sequence(3));    // [1, 2, 3]
+//console.log(sequence(1));    // [1]
 // The below code connects this JS file to our test file
 module.exports = {
   isRealPalindrome,
--- exercises.test.js
+++ exercises.test.js
@@ -1,4 +1,4 @@
-const exercises = require('./exercises');
+const exercises = require('./exercises.js');
 
 //write tests in this file
 
@@ -8,3 +8,55 @@ test('Question 0: add', () => {
   expect(exercises.add(1, -1)).toEqual(0);
 });
 */
+test('Palandrome function: add', () => {
+  expect(exercises.isRealPalindrome('madam')).toEqual(true);
+  expect(exercises.isRealPalindrome('Madam')).toEqual(true);
+  expect(exercises.isRealPalindrome("Madam, I'm Adam")).toEqual(true);
+  expect(exercises.isRealPalindrome('Madam')).toEqual(true);
+  expect(exercises.isRealPalindrome('Madam')).toEqual(true);
+
+});
+
+test('RunningTotal function', () => {
+  expect(exercises.runningTotal([2, 5, 13])).toEqual([2, 7, 20]);
+  expect(exercises.runningTotal([14, 11, 7, 15, 20])).toEqual([14, 25, 32, 47, 67]);
+  expect(exercises.runningTotal([3])).toEqual([3]);
+  expect(exercises.runningTotal([])).toEqual([])
+});
+test('swap function', () => {
+  expect(exercises.swap('Oh what a wonderful day it is')).toBe('hO thaw a londerfuw yad ti si');
+  expect(exercises.swap('Abcde')).toBe('ebcdA');
+  expect(exercises.swap('a')).toBe('a');
+
+})
+test('wordSizes function ', () => {
+  expect(exercises.wordSizes('Four score and seven.')).toEqual({ "3": 1, "4": 1, "5": 1, "6": 1 });
+  expect(exercises.wordSizes("What's up doc?")).toEqual({ "2": 1, "4": 1, "6": 1 });
+  expect(exercises.wordSizes("I ate her lunch")).toEqual({ "1": 1, "3": 2, "5": 1 });
+});
+test('union function', () => {
+  expect(exercises.union([1, 3, 5], [3, 6, 9])).toEqual([1, 3, 5, 6, 9]);
+  expect(exercises.union([2, 2, 2, 2], [10, 5, 2])).toEqual([2, 10, 5]);
+});
+test('firstRecurring function ', () => {
+  expect(exercises.firstRecurring('reuben')).toBe("e");
+  expect(exercises.firstRecurring('anne')).toBe("n");
+  expect(exercises.firstRecurring('restaurant')).toBe("r");
+  expect(exercises.firstRecurring('paul')).toBe("");
+
+});
+test('showMultiplicativeAverage function', () => {
+  expect(exercises.showMultiplicativeAverage([3, 5])).toBe("7.500");
+  expect(exercises.showMultiplicativeAverage([2, 5, 7, 11, 13, 17])).toBe("28361.667");
+
+});
+test('multiplyList function', () => {
+  expect(exercises.multiplyList([3, 5, 7], [9, 10, 11])).toEqual([27, 50, 77]);
+  expect(exercises.multiplyList([5, 10, 15, 20], [1, 2, 3, 4])).toEqual([5, 20, 45, 80]);
+});
+test('sequence function', () => {
+  expect(exercises.sequence(5)).toEqual([1, 2, 3, 4, 5]);
+  expect(exercises.sequence(3)).toEqual([1, 2, 3]);
+  expect(exercises.sequence(1)).toEqual([1]);
+});
+
